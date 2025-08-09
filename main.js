cb5a6f4c35594792c82260adff959c8d9c04b6ab
اسمك هنا < >
2025-08-09 13:17:01 +0300

    .

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("welcomey to java script"); // امر طباعة
+let age = 15; // تعريف متغير العمر
+console.log(age);
+console.log(age);
+console.log(age);
+console.log(age);
+
+age = 20; // تغيير قيمة المتغير
+console.log(age);
+
+const name = 'Abdulrahman'; // تعريف ثابت الاسم
+let age2 = 17; // تعريف متغير العمر الثاني
+
+console.log(name);
+console.log(age2);
+
+let name2 = 'Salih'; // string
+let age3 = 20; // number
+
+console.log(typeof name2);
+console.log(typeof age3);
+
+let result = 2 > 3;
+console.log(result); // boolean
+
+console.log(typeof result);
+
+let language = 'Python';
+let message = `I love ${language}`; // استخدام القالب النصي
+console.log(message);
+
+let value;
+console.log(value); // undefined غير معرف
+

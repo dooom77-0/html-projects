5945c947da38b40da0e94d496771c9eb111dacde
اسمك هنا < >
2025-08-22 13:37:34 +0300

    .

--- javascript/loops.js
+++ javascript/loops.js
@@ -4,3 +4,43 @@ for(let i = 1; i <= 100; i++){
 for(let x = 100; x >= 2; x-=2){
     console.log(x);
 }
+
+const colors = ['red' , 'green' , 'blue' , 'black' , 'white','purple'];
+
+for(let color of colors){
+    console.log(color);
+}
+
+const message = 'Welcome to Javascript';
+
+for(let char of message){
+    console.log(char);
+}
+
+const colorss = ['red' , 'green' , 'blue' , 'black' , 'white','purple'];
+
+let color = 0;
+
+while(color < colorss.length){ // while loop
+    console.log(colorss[color]);
+    color++;
+}
+
+const numbers = [1,2,3,4,0,5,6,7]
+
+for(let i = 0 ;i < numbers.length; i++){
+    if(numbers[i] == 0){
+        break; // Stop the loop
+    }
+    console.log(numbers[i]);
+}
+
+
+const numberss = [1,2,3,4,0,5,6,7]
+
+for(let i = 0 ;i < numberss.length; i++){
+    if(numberss[i] == 0){
+        continue; // Skip the current iteration
+    }
+    console.log(numberss[i]);
+}

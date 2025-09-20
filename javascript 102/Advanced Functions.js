5cf3161644ac55db125ee4bb666c97170aa1ac1f
اسمك هنا < >
2025-09-20 18:04:53 +0300

    .

--- javascript 102/Advanced Functions.js
+++ javascript 102/Advanced Functions.js
@@ -16,3 +16,45 @@ printName = function(age){
     console.log('Hello,'+this.firstName+' '+this.lastName+ '.your age is :'+age);
 }
 printName.apply(user2,[25]) // apply method
+
+let add = (num1 , num2) => num1 + num2 // Arrow functions =>
+console.log(add(5,3))
+
+let obj = {
+    title : 'How to write a function in an object',
+
+    function1 : function(){
+        return console.log('this is function1')
+    },
+
+    function2 : () => {
+        return console.log('this is function2')
+    },
+
+    function3(){
+        return console.log('this is function3')
+    }
+}
+obj.function1()
+obj.function2()
+obj.function3()
+
+
+function areas(w,l=10){
+    let area = w * l;
+    return area;
+
+}
+let area = areas(5,6);
+console.log(area)
+
+
+let studentsnames = (name1,name2,...names) => console.log(name1,name2,names) //*Rest parameters//
+
+studentsnames('Khalid','Salim','Mohammed','Hassoun','Ali')
+
+
+let StudentsNames = (name1,name2,name3,name4) => console.log(name1,name2,name3,name4)
+
+names = ['Ali','Mohamed','Salih','Mousa']
+StudentsNames(...names) //*Spread Operator//
---
+++ javascript 102/Unit project.js
@@ -0,0 +1,13 @@
+class Vehicle{
+    constructor(name,company,ID){
+        this.name = name;
+        this.company = company;
+        this.ID = ID;
+    }
+}
+class Car extends Vehicle{
+    constructor(car,gasorelectric){
+        this.car = car;
+        this
+    }
+}

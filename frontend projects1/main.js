31534f29a5d5e3ec99f8992377efafc727c0d00f
اسمك هنا < >
2025-09-30 07:25:38 +0300

    ..

--- frontend projects1/main.js
+++ frontend projects1/main.js
@@ -2,15 +2,14 @@ function Github(){
     window.open('https://github.com/dooom77-0/py-projects/tree/main/%D9%85%D8%B4%D8%A7%D8%B1%D9%8A%D8%B9%20%D8%A8%D8%A7%D9%8A%D8%AB%D9%88%D9%86','_blank')
 }
 
-function opinion(){
-  let opinion = prompt('What is your opinions?');
+function Suggest() {
+    let opinion = prompt("Give me your suggestions for the site");
 
-  if(opinion){
-    localStorage.setItem('useropinion',opinion)
-
-    document.getElementById('feedback').innerText = '(Thanks for feedback)';
+    if (opinion && opinion.trim() !== "") {
+      document.getElementById("feedback").innerText = 'Thanks for your feedback';
+      document.getElementById("su-btn1").style.display = "none";
+    } else {
+      document.getElementById("feedback").innerText = "No suggestions have been entered.";
+    }
+ 
   }
-  else{
-    document.getElementById('feedback').innerText = 'No opinion has been entered'
-  }
-}

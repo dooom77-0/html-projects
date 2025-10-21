16699b3b8f6225da6ef320ae2bd1b1f0869f38fd
اسمك هنا < >
2025-10-21 13:43:38 +0300

    //zz/z/z/z

--- my own website 👨‍💻/main.js
+++ my own website 👨‍💻/main.js
@@ -1,6 +1,12 @@
-function Github(){
-    window.open('https://github.com/dooom77-0/py-projects/tree/main/%D9%85%D8%B4%D8%A7%D8%B1%D9%8A%D8%B9%20%D8%A8%D8%A7%D9%8A%D8%AB%D9%88%D9%86','_blank')
-}
+const mainBtn = document.getElementById('main-btn');
+mainBtn.addEventListener('click', () => {
+  window.open('https://github.com/dooom77-0/py-projects/tree/main/%D9%85%D8%B4%D8%A7%D8%B1%D9%8A%D8%B9%20%D8%A8%D8%A7%D9%8A%D8%AB%D9%88%D9%86','_blank')
+});
+
+const mainBtn2 = document.getElementById('main-btn-2');
+mainBtn2.addEventListener('click', () => {
+  window.open('https://github.com/dooom77-0/html-projects','_blank')
+});
 
 function Suggest() {
   alert('Note: This is my first project in html , css and javascript!')
